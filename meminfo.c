aa5c01318b2933ecba6f999a896580454d5e6845
Benoit Jacquemont <benoit@akeneo.com>
2014-02-13 08:44:25 +0100

    Function now uses stream handle

--- meminfo.c
+++ meminfo.c
@@ -17,6 +17,7 @@
 const zend_function_entry meminfo_functions[] = {
     PHP_FE(meminfo_structs_size, NULL)
     PHP_FE(meminfo_objects_list, NULL)
+    PHP_FE(meminfo_gc_roots_list, NULL)
     {NULL, NULL, NULL}
 };
 
@@ -43,22 +44,36 @@ ZEND_GET_MODULE(meminfo)
 
 PHP_FUNCTION(meminfo_structs_size)
 {
-    zend_printf("==============================\n");
-    zend_printf("Structs size on this platform:\n");
-    zend_printf("------------------------------\n");
-    zend_printf("  Class: %ld bytes.\n", sizeof( zend_class_entry));
-    zend_printf("  Object: %ld bytes.\n", sizeof( zend_object));
-    zend_printf("  Variable: %ld bytes.\n", sizeof( zval));
-    zend_printf("  Variable value: %ld bytes.\n", sizeof( zvalue_value));
-    zend_printf("===============================\n\n");
+    zval *zval_stream;
+    php_stream *stream;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zval_stream) == FAILURE) {
+        return;
+    }
+
+    php_stream_from_zval(stream, &zval_stream);
+
+    stream_printf(stream, "Structs size on this platform:\n");
+    stream_printf(stream, "  Class (zend_class_entry): %ld bytes.\n", sizeof(zend_class_entry));
+    stream_printf(stream, "  Object (zend_object): %ld bytes.\n", sizeof(zend_object));
+    stream_printf(stream, "  Variable (zval): %ld bytes.\n", sizeof(zval));
+    stream_printf(stream, "  Variable value (zvalue_value): %ld bytes.\n", sizeof(zvalue_value));
 }
 
 PHP_FUNCTION(meminfo_objects_list)
 {
-    zend_printf("===============================\n");
-    zend_printf("Dumping objects list:\n");
-    zend_printf("-------------------------------\n");
+    zval *zval_stream;
+    php_stream *stream;
 
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zval_stream) == FAILURE) {
+        return;
+    }
+
+    php_stream_from_zval(stream, &zval_stream);
+
+    stream_printf(stream, "Objects list:\n");
+
+// TODO: check if object_buckets exists ? See gc_collect_roots from zend_gc.c
     zend_objects_store *objects = &EG(objects_store);
     zend_uint i;
     zend_uint total_objects_buckets = objects->top - 1;
@@ -72,14 +87,42 @@ PHP_FUNCTION(meminfo_objects_list)
             object =  (zend_object * ) obj->object;
 
             class_entry = object->ce;
-            zend_printf("  Object handle %d, of type %s.\n", i, class_entry->name);
+            stream_printf(stream, "  - Class %s, handle %d\n", class_entry->name, i);
 
             current_objects++;
         }
      }
 
-    zend_printf("Objects buckets: %d. Current objects: %d.\n", total_objects_buckets, current_objects);
-    zend_printf("===============================\n\n");
+    stream_printf(stream, "Total object buckets: %d. Current objects: %d.\n", total_objects_buckets, current_objects);
+
+}
+
+PHP_FUNCTION(meminfo_gc_roots_list)
+{
+    zval *zval_stream;
+    php_stream *stream;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zval_stream) == FAILURE) {
+        return;
+    }
+
+    php_stream_from_zval(stream, &zval_stream);
+    stream_printf(stream, "GC roots list:\n");
+
+    gc_root_buffer *current = GC_G(roots).next;
+    while (current != &GC_G(roots)) {
+        if (current->handle) {
+            stream_printf(stream, "  Handle %d found on GC root\n", current->handle);
+        } else {
+            zval* z =current->u.pz;
+
+            stream_printf(stream, "  Not object GC root\n");
+            stream_printf(stream, "  GC root of type %c\n",Z_TYPE_P(current->u.pz));
+        }
+        stream_printf(stream, "    Ref count GC %d\n",current->u.pz->refcount__gc);
+        current = current->next;
+
+    }
 }
 
 
--- php_meminfo.h
+++ php_meminfo.h
@@ -4,11 +4,12 @@
 #define MEMINFO_NAME "MemInfo"
 #define MEMINFO_VERSION "0.1-dev"
 #define MEMINFO_AUTHOR "Benoit Jacquemont"
-#define XDEBUG_COPYRIGHT  "Copyright (c) 2010-2011 by Benoit Jacquemont"
-#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2010-2011"
+#define XDEBUG_COPYRIGHT  "Copyright (c) 2010-2014 by Benoit Jacquemont"
+#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2010-2014"
 
-PHP_FUNCTION(meminfo_objects_list);
 PHP_FUNCTION(meminfo_structs_size);
+PHP_FUNCTION(meminfo_objects_list);
+PHP_FUNCTION(meminfo_gc_roots_list);
 
 extern zend_module_entry meminfo_entry;
 #define phpext_meminfo_ptr &meminfo_module_entry
